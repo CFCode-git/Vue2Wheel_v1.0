39ea17b49fad582cf0e19ed51c503adc82752274
CFCode-git <435184766@qq.com>
2020-10-26 12:47:03 +0800

    创建 button-group.vue 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import buttonGroup from './button-group.vue'
 
+// 全局注册
 Vue.component('diff-button', Button)
 Vue.component('diff-icon', Icon)
+Vue.component('diff-button-group', buttonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="diff-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'buttonGroup',
+  }
+</script>
+<style lang="scss">
+  .diff-button-group {
+    display: inline-flex;
+    > .diff-button {
+      border-radius: 0;
+      margin-left:-1px;
+      &:first-child {
+        border-top-left-radius: var(--button-radius);
+        border-bottom-left-radius: var(--button-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--button-radius);
+        border-bottom-right-radius: var(--button-radius);
+      }
+      &:hover{
+        position: relative;
+        z-index:1;
+      }
+    }
+  }
+</style>
