855d8fede1b979acb84a99b4d93bc47c2a14f6a7
CFCode-git <435184766@qq.com>
2020-10-27 23:10:21 +0800

    创建Input组件，样式基本完成，下一步添加事件监听

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
 import buttonGroup from './button-group.vue'
+import Input from './input.vue'
 
 // 全局注册
 Vue.component('diff-button', Button)
 Vue.component('diff-icon', Icon)
 Vue.component('diff-button-group', buttonGroup)
+Vue.component('diff-input', Input)
 
 new Vue({
   el: '#app',
---
+++ src/input.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper" :class="{'error':errorMessage}">
+    <input
+      type="text"
+      :value="value"
+      :disabled="disabled"
+      :readonly="readonly"
+    >
+    <template v-if="errorMessage">
+      <diff-icon icon-name="warning" class="icon-warning"></diff-icon>
+      <span class="errorMessage">{{errorMessage}}</span>
+    </template>
+  </div>
+</template>
+<script>
+  import Icon from './icon.vue'
+
+  export default {
+    name: 'diffInput',
+    components: {'diff-icon': Icon},
+    props: {
+      value: {type: String},
+      disabled: {type: Boolean, default: false},
+      readonly: {type: Boolean, default: false},
+      errorMessage: {type: String}
+
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  $height: 32px;
+  $border-color: #999;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $padding: 0 8px;
+  $font-size: 12px;
+  $box-shadow-color: rgba(0, 0, 0, .5);
+  $error-color: #f1453d;
+  $tips-color: #8f97f1;
+  .wrapper {
+    font-size: $font-size;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    > :not(:last-child) {margin-right: .5em;}
+    > input {
+      height: $height;
+      border: 1px solid $border-color;
+      border-radius: $border-radius;
+      padding: $padding;
+      font-size: inherit;
+      &:hover {border-color: $border-color-hover; }
+      &:focus { outline: none; box-shadow: inset 0 1px 3px $box-shadow-color; }
+      &[disabled], &[readonly] {
+        border-color: #bbb;
+        color: #bbb;
+        cursor: not-allowed;
+      }
+    }
+    &.error {
+      > input {
+        border-color: $error-color;
+      }
+    }
+    .icon-warning {
+      fill: $error-color;
+    }
+    .errorMessage {
+      color: $error-color;
+    }
+  }
+</style>
