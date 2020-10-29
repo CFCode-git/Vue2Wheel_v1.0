b54a210cbadee9796e5883460e34a26741a433f6
CFCode-git <435184766@qq.com>
2020-10-29 09:46:10 +0800

    接受 gutter 属性

--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2 + 'px',marginRight:-gutter/2 + 'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    name:"diff-row"
+    name: 'diff-row',
+    props: {gutter: {type: [Number, String]}},
+    /* created 创建对象/组件, 但是还没挂载到页面, mounted 已经挂载到页面;
+    *  Vue 父子组件钩子顺序
+    *  顺序: row created => col created => col mounted => row mounted
+    * */
+    mounted() {
+      console.log(this.$children)
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+      })
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-  .row{
+  .row {
     display: flex;
   }
 </style>
