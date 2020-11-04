342e5dd85f12ffd8094f8c4844a867788c4b4ea6
CFCode-git <435184766@qq.com>
2020-11-04 16:13:35 +0800

    添加 line 动画

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="tabs-head">
-   <slot></slot>
-    <div class="line" ref="lineRef"></div>
+    <slot></slot>
+    <div class="line" ref="lineRef" v-if="x"></div>
+    <!--    v-if 控制div是否显示在dom里-->
     <div class="actions-wrapper">
       <slot name="actions"></slot>
     </div>
@@ -11,31 +12,44 @@
   export default {
     name: 'diff-tabs-head',
     inject: ['eventBus'],
-    created() {
-      this.eventBus.$on('update:selected',(item,vm)=>{
-        console.log(item)
+    data() {
+      return {
+        x: false
+      }
+    },
+    mounted() {
+      this.eventBus.$on('update:selected', (item, vm) => {
+        this.x = true
+        // 当 x 变为 true， line 不会马上出现在页面，而会新增一个更新 ui 任务到任务队列，
+        // 等下面代码执行完了再更新 ui，所以需要用 nextTick 包起来
+        this.$nextTick(() => {
+          // nextTick 会新增一个函数，把下面的操作放到任务队列里面
+          let {width, left} = vm.$el.getBoundingClientRect()
+          this.$refs.lineRef.style.width = `${width}px`
+          this.$refs.lineRef.style.transform = `translateX(${left}px)` // 使用 transform可以做硬件 3d 加速
+        })
+
       })
     },
-    mounted() { }
   }
 </script>
 <style lang="scss" scoped>
   $tab-height:40px;
   $blue:blue;
-  .tabs-head{
+  .tabs-head {
     display: flex;
-    height:$tab-height;
+    height: $tab-height;
     justify-content: flex-start;
-    border:1px solid red;
+    border: 1px solid red;
     position: relative;
-    >.line{
+    > .line {
       position: absolute;
-      bottom:0;
-      border-bottom: 1px solid $blue;
-      width:100px;
+      bottom: 0;
+      border-bottom: 3px solid $blue;
+      transition: all 400ms;
     }
-    >.actions-wrapper{
-      margin-left:auto;
+    > .actions-wrapper {
+      margin-left: auto;
     }
   }
 </style>
--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -30,7 +30,7 @@
     },
     methods: {
       xxx() {
-        this.eventBus.$emit('update:selected', this.name)
+        this.eventBus.$emit('update:selected', this.name,this)
       }
     }
   }
