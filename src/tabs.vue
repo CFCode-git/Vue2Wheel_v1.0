0c334ce0303c7c561fbb842781ca4db39a1ea4d5
CFCode-git <435184766@qq.com>
2020-11-07 01:06:05 +0800

    小重构

--- src/tabs.vue
+++ src/tabs.vue
@@ -26,23 +26,28 @@
     },
     created() {
     },
-    mounted() { // mounted 保证所有子元素 全部创建完毕
-      // 检查 children 类型
-      if (this.$children.length === 0) { // $children 只能获取到子组件（比如tabs-head），不能获取到子元素（比如div）
-        console && console.warn && console.warn('tabs子组件应该为tabs-head和tabs-body，但你没有写子组件')
-      }
-      // this.$emit('update:selected', '这是this $emit 出来的事件')
-      this.$children.forEach(vm => { // 筛选 vm
-        if (vm.$options.name === 'diff-tabs-head') {
-          vm.$children.forEach(childVm => {
-            if (childVm.$options.name === 'diff-tabs-item' && childVm.name === this.selected) {
-              this.eventBus && this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
+    methods: {
+      checkChildren() {
+        // 检查 children 类型
+        if (this.$children.length === 0) { // $children 只能获取到子组件（比如tabs-head），不能获取到子元素（比如div）
+          console && console.warn && console.warn('tabs子组件应该为tabs-head和tabs-body，但你没有写子组件')
         }
-      })
-      // this.$emit('update:selected','xxx')
-
+      },
+      selectTab() {
+        this.$children.forEach(vm => { // 筛选 vm
+          if (vm.$options.name === 'diff-tabs-head') {
+            vm.$children.forEach(childVm => {
+              if (childVm.$options.name === 'diff-tabs-item' && childVm.name === this.selected) {
+                this.eventBus && this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted() { // mounted 保证所有子元素 全部创建完毕
+      this.checkChildren()
+      this.selectTab()
     },
   }
 </script>
