e310588fb9b88de7e13e302bff295597b12a9d95
CFCode-git <435184766@qq.com>
2020-11-07 01:40:56 +0800

    添加默认样式，使用filter：drop-shadow为popover整体添加shadow

--- src/popover.vue
+++ src/popover.vue
@@ -29,7 +29,9 @@
       // 处理点击 document 的情况，同时避开点击 content 的情况
       onClickDocument(e) {
         // 如果点击的是 popover 或者 popover 的 content,就拜拜
-        if (this.$refs.popover.contains(e.target) || this.$refs.contentWrapperRef.contains(e.target)) { return }
+        // if (this.$refs.popover.contains(e.target) || this.$refs.contentWrapperRef.contains(e.target)) { return }
+        if (this.$refs.popover && this.$refs.popover === e.target || this.$refs.popover.contains(e.target)) { return }
+        if (this.$refs.contentWrapperRef && this.$refs.contentWrapperRef === e.target || this.$refs.contentWrapperRef.contains(e.target)) { return }
         this.close()
       },
       open() {
@@ -55,6 +57,8 @@
   }
 </script>
 <style lang="scss" scoped>
+  $border-color: #333;
+  $border-radius: 4px;
   .popover {
     display: inline-block;
     vertical-align: top;
@@ -62,8 +66,33 @@
   }
   .content-wrapper {
     position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    /*box-shadow: 0 0 3px rgba(0, 0, 0, .5);*/
+    /* drop-shadow + background 可以使 popover 和 小三角形 都有 box-shadow 的阴影，但兼容性可能不太好，可以用 caniuse.com 查看 */
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+    background: white;
     transform: translateY(-100%);
+    padding: .5em 1em;
+    margin-top: -10px;
+    max-width: 20em; /*默认最大宽度：最多显示20个字*/
+    word-break: break-all; /*英文网站最好不要加这个样式*/
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      height: 0;
+      width: 0;
+      position: absolute;
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
   }
 </style>
