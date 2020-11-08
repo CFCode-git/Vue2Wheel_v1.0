d91143d09970883615c541978dde051b9261330f
CFCode-git <435184766@qq.com>
2020-11-08 22:52:17 +0800

    解决小三角遮挡button导致popover内容抖动的问题

--- src/popover.vue
+++ src/popover.vue
@@ -143,27 +143,27 @@
       transform: translateY(-100%);
       margin-top: -10px;
       &::before, &::after { left: 10px; }
-      &::before { border-top-color: black; top: 100%; }
-      &::after { border-top-color: white; top: calc(100% - 1px); }
+      &::before { border-top-color: black;border-bottom:none; top: 100%; }
+      &::after { border-top-color: white;border-bottom:none; top: calc(100% - 1px); }
     }
     &.position-bottom {
       margin-top: 10px;
       &::before, &::after { left: 10px; }
-      &::before { border-bottom-color: black; bottom: 100%; }
-      &::after {border-bottom-color: white; bottom: calc(100% - 1px); }
+      &::before { border-bottom-color: black;border-top: none; bottom: 100%; }
+      &::after {border-bottom-color: white;border-top: none; bottom: calc(100% - 1px); }
     }
     &.position-left {
       transform: translateX(-100%);
       margin-left: -10px;
       &::before, &::after { transform: translateY(-50%); top: 50%; }
-      &::before { border-left-color: black; left: 100%; }
-      &::after { border-left-color: white; left: calc(100% - 1px); }
+      &::before { border-left-color: black;border-right:none; left: 100%; }
+      &::after { border-left-color: white;border-right:none; left: calc(100% - 1px); }
     }
     &.position-right {
       margin-left: 10px;
       &::before, &::after { transform: translateY(-50%); top: 50%; }
-      &::before { border-right-color: black; right: 100%; }
-      &::after { border-right-color: white; right: calc(100% - 1px); }
+      &::before { border-right-color: black;border-left:none; right: 100%; }
+      &::after { border-right-color: white;border-left:none; right: calc(100% - 1px); }
     }
   }
 </style>
