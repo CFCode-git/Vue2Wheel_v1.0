1a9f424ce53b7b10737ea69773745a20ece02ab9
CFCode-git <435184766@qq.com>
2020-11-13 11:47:57 +0800

    优化：页面上只有一个document click监听器

--- src/click-outside.js
+++ src/click-outside.js
@@ -1,15 +1,28 @@
 // 自定义指令：用于封装 DOM 操作
 // 不需要 removeEventListener 因为 bind 只会在第一次初始化的时候监听，不会重复监听。
+let onClickDocument = (e) => {
+  let {target} = e
+  callbacks.forEach(item => {
+    //item.el item.callback
+    if (item.el === target || item.el.contains(target)) {
+      return
+    } else {
+      item.callback()
+    }
+  })
+}
+document.addEventListener('click', onClickDocument)
+
+let callbacks = []
+
 export default {
   bind: function (el, binding, vnode) {
-    document.addEventListener('click', e => {
-      let {target} = e
-      if (el === target || el.contains(target)) {
-        console.log('inside')
-        return
-      }
-      console.log('outside')
-      binding.value()
-    })
+    callbacks.push({el, callback: binding.value})
   }
 }
+
+let removeListener = () => {
+  document.removeEventListener('click', onClickDocument)
+}
+export {removeListener}
+
