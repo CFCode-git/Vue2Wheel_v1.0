e0235aed5c5f78f24ea958cc4c6644c972aae26d
CFCode-git <435184766@qq.com>
2020-11-16 10:04:37 +0800

    新建slides slides-item组件，使用 vue 动画 transition，初步实现自动轮播功能

---
+++ src/slides-item.vue
@@ -0,0 +1,45 @@
+<template>
+  <transition name="slide" mode="out-in">
+    <div class="slides-item" v-if="visible">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+<script>
+  export default {
+    name: 'diff-slides-item',
+    props: {
+      name: {type: String, required: true}
+    },
+    data() {
+      return {selected: undefined}
+    },
+    computed: {
+      visible() {
+        return this.selected === this.name
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .slide-leave-active {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+  .slide-enter-active, .slide-leave-active {
+    transition: all 1s;
+  }
+  .slide-enter {
+    transform: translateX(100%);
+  }
+  .slide-leave-to {
+    transform: translateX(-100%);
+  }
+  /*.slides-item {*/
+  /*  position: absolute;*/
+  /*  top:0;*/
+  /*  left:0;*/
+  /*}*/
+</style>
+
---
+++ src/slides.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="diff-slides">
+    <div class="diff-slides-window" ref="window">
+      <div class="diff-slides-wrapper">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'diff-slides',
+    props: {
+      selected: {type: String},
+    },
+    mounted() { this.updateChildren() },
+    updated() { this.updateChildren() },
+    methods: {
+      updateChildren() {
+        const first = this.$children[0]
+        const selected = this.selected || first.name
+        this.$children.forEach(vm => {
+          vm.selected = selected
+        })
+      },
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .diff-slides {
+    display: inline-block;
+    &-wrapper {
+      position: relative;
+    }
+    &-window { }
+  }
+</style>
