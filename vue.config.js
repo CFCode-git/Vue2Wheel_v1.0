67344c90ed34cbb70514bd1536ddb7b3bca8be9d
CFCode-git <435184766@qq.com>
2020-11-18 01:06:35 +0800

    移动 scss 至 styles 目录

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
       sass: {
         implementation: require('sass'), // 如果要使用dart-sass要加这个选项 https://github.com/webpack-contrib/sass-loader
         sassOptions: {
-          includePaths: [path.join(__dirname, 'src')]
+          includePaths: [path.join(__dirname, 'styles')]
         },
       }
     }
