b0f252f3052c2c8e4efea1df97a265fee6ef55f7
CFCode-git <435184766@qq.com>
2020-11-24 00:33:41 +0800

    新建table组件,完成单选功能

--- src/demo.vue
+++ src/demo.vue
@@ -1,19 +1,53 @@
 <template>
-  <div style="margin:20px;">
-    <diff-pager :total-page="1" :current-page.sync="currentPage" :hide-if-one-page="false"></diff-pager>
-
+  <div>
+    {{selected}}
+    <div style="margin: 20px;">
+      <diff-table :columns="columns" :data-source="dataSource" bordered @changeItem="x"></diff-table>
+    </div>
+    <div style="margin: 20px;">
+      <diff-table :columns="columns" :data-source="dataSource" compact :striped="false"></diff-table>
+    </div>
+    <div style="margin:20px;">
+      <diff-pager :total-page="1" :current-page.sync="currentPage" :hide-if-one-page="false"></diff-pager>
+    </div>
   </div>
 </template>
 
 <script>
   import DiffPager from './pager/pager'
+  import DiffTable from './table/table'
 
   export default {
     name: 'demo',
-    components: {DiffPager},
+    components: {DiffPager, DiffTable},
     data() {
       return {
-        currentPage: 1
+        currentPage: 1,
+        selected: [],
+        dataSource: [
+          {id: 1, name: 'Chow', score: 100},
+          {id: 2, name: 'Jack', score: 80},
+          {id: 3, name: 'Jane', score: 60},
+          {id: 4, name: 'Joshua', score: 40},
+          {id: 5, name: 'Amy', score: 40},
+          {id: 6, name: 'Alice', score: 40},
+          {id: 7, name: 'Bob', score: 40},
+        ],
+        columns: [
+          {text: '姓名', field: 'name'},
+          {text: '分数', field: 'score'},
+        ]
+      }
+    },
+    methods: {
+      x(object) {
+        let {selected, item, index} = object
+        if (selected) {
+          this.selected.push(item)
+        } else {
+          let index = this.selected.indexOf(item)
+          this.selected.splice(index, 1)
+        }
       }
     }
   }
---
+++ src/table/table.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="diff-table-wrapper">
+    <table class="diff-table" :class="{bordered,compact,striped}">
+      <thead>
+      <tr>
+        <th><input type="checkbox"></th>
+        <th v-if="numberVisible">#</th>
+        <th v-for="column in columns">{{column.text}}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item,index) in dataSource">
+        <td v-if="numberVisible">{{index + 1}}</td>
+        <td><input type="checkbox" @change="onChangeItem(item,index,$event)"></td>
+        <template v-for="column in columns">
+          <td>{{item[column.field]}}</td>
+        </template>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'diff-table',
+    props: {
+      columns: {type: Array, required: true},
+      dataSource: {type: Array, required: true},
+      numberVisible: {type: Boolean, default: false},
+      bordered: {type: Boolean, default: false},
+      compact: {type: Boolean, default: false},
+      striped: {type: Boolean, default: true},
+    },
+    methods: {
+      onChangeItem(item, index, e) {
+        this.$emit('changeItem', {selected: e.target.checked, item, index})
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import 'var';
+  $grey: darken($grey, 10%);
+  .diff-table {
+    border-bottom: 1px solid $grey;
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+    &.bordered {
+      border: 1px solid $grey;
+      td, th {
+        border: 1px solid $grey;
+      }
+    }
+    &.compact {
+      td, th {
+        padding: 4px;
+      }
+    }
+    &.noStriped {
+    }
+    td, th {
+      border-bottom: 1px solid $grey;
+      text-align: left;
+      padding: 8px;
+    }
+    &.striped {
+      tbody {
+        > tr {
+          &:nth-child(odd) {
+            background: white;
+          }
+          &:nth-child(even) {
+            background: lighten($grey, 10%);
+          }
+        }
+      }
+    }
+  }
+</style>
