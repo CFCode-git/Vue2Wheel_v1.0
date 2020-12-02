f2d75dbd175ef4b7077fed5d6887b30aeb499515
CFCode-git <435184766@qq.com>
2020-12-02 14:31:48 +0800

    创建入口文件index.js,使用vue-cli的 lib 模式

--- vue.config.js
+++ vue.config.js
@@ -10,20 +10,4 @@ module.exports = {
       }
     }
   }
-  // chainWebpack: (config) => { // https://github.com/vuetifyjs/vue-cli-plugins/issues/101
-  //   if (process.env.NODE_ENV === 'test') {
-  //     const sassRule = config.module.rule('sass')
-  //     sassRule.uses.clear()
-  //     sassRule.use('null-loader').loader('null-loader')
-  //     const scssRule = config.module.rule('scss')
-  //     scssRule.uses.clear()
-  //     scssRule.use('null-loader').loader('null-loader')
-  //   }
-  // Allow to mix SASS and SCSS
-  // https://vuetifyjs.com/en/customization/sass-variables#single-file-components
-  // ['vue-modules', 'vue', 'normal-modules', 'normal'].forEach((match) => {
-  //   config.module.rule('scss').oneOf(match).use('sass-loader')
-  //     .tap(opt => Object.assign(opt, {data: '@import "~@/styles/application.scss";'}))
-  // })
-  // },
 }
